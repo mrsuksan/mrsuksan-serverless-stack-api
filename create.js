4253dd623d026282bbc90ab8e84e976b1d824472
Suksan Jirachaweng <suksanjirachaweng@Suksans-MacBook-Pro.local>
2020-04-17 13:26:17 +0700

    test

--- create.js
+++ create.js
@@ -2,7 +2,7 @@ import * as uuid from "uuid";
 import AWS from "aws-sdk";
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
+// Test
 export function main(event, context, callback) {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
