407acf7ce408a56dfa274cc51f661e89b7a59bfc
Suksan Jirachaweng <suksanjirachaweng@Suksans-MacBook-Pro.local>
2020-04-17 13:23:05 +0700

    Add create

--- create.js
+++ create.js
@@ -6,11 +6,11 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 export function main(event, context, callback) {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
-
+  console.log(data);
   const params = {
     TableName: process.env.tableName,
     // 'Item' contains the attributes of the item to be created
-    // - 'userId': user identities are federated through the
+    // - 'userId': user identities are federated through thes
     //             Cognito Identity Pool, we will use the identity id
     //             as the user id of the authenticated user
     // - 'noteId': a unique uuid
@@ -27,6 +27,7 @@ export function main(event, context, callback) {
   };
 
   dynamoDb.put(params, (error, data) => {
+    console.log(error);
     // Set response headers to enable CORS (Cross-Origin Resource Sharing)
     const headers = {
       "Access-Control-Allow-Origin": "*",
